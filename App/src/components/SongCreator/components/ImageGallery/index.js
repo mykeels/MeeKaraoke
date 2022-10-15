5d191e8be101162397313cfa244f14e28ab4b053
mykeels <mykehell123@gmail.com>
2022-10-15 14:01:24 -0400

    feat: show image sliders in gallery

--- App/src/components/SongCreator/components/ImageGallery/index.js
+++ App/src/components/SongCreator/components/ImageGallery/index.js
@@ -1,5 +1,7 @@
+import "./ImageGallery.css";
+
 import classNames from "classnames";
-import React from "react";
+import React, { useEffect } from "react";
 
 /**
  * @param {object} props
@@ -14,23 +16,68 @@ export const ImageGallery = ({ cursor, images, line }) => {
     (Math.floor(cursor) + 5 - (Math.floor(cursor) % 5)) / 5 - 1
   );
   const current = images[index];
+
+  useEffect(() => {
+    /** @type {HTMLDivElement} */
+    const parentElem = document.querySelector(".image-slider-wrapper");
+    /** @type {HTMLDivElement} */
+    const elem = document.querySelector(`.gallery-image-${index}`);
+    if (elem) {
+      const left =
+        elem?.offsetLeft - parentElem?.clientWidth / 2 + elem?.clientWidth / 2;
+      parentElem.scrollTo({
+        left,
+        behavior: "smooth"
+      });
+    }
+  }, [cursor, index]);
+
   return (
-    <div className="flex bg-pink preview relative">
-      <img
-        src={current}
-        className={classNames("block w-full h-full rounded border-2", {
-          "border-purple-100": !cursor,
-          "border-purple-200": cursor > 0
-        })}
-      />
-      {line?.text ? (
-        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-          <div className="h-24 relative w-full text-center flex items-center justify-center">
-            <div className="h-24 bg-gray-500 w-full z-0 opacity-50 rounded absolute top-0 left-0" />
-            <div className="z-10 relative text-white text-sm font-bold p-4">{line.text}</div>
+    <div>
+      <div className="flex bg-pink preview relative">
+        <img
+          src={current}
+          className={classNames("block w-full h-full rounded border-2", {
+            "border-purple-100": !cursor,
+            "border-purple-200": cursor > 0
+          })}
+        />
+        {line?.text ? (
+          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+            <div className="h-24 relative w-full text-center flex items-center justify-center">
+              <div className="h-24 bg-gray-500 w-full z-0 opacity-50 rounded absolute top-0 left-0" />
+              <div className="z-10 relative text-white text-sm font-bold p-4">
+                {line.text}
+              </div>
+            </div>
           </div>
+        ) : null}
+      </div>
+      <div className="py-2"></div>
+      <div className="block rounded overflow-auto image-slider-wrapper">
+        <div
+          className="block image-slider"
+          style={{ width: `${images.length * 176}px` }}
+        >
+          <ul className="list-none">
+            {images.map((image, i) => (
+              <li key={`${image}-${i}`} className="inline-block py-1 px-1">
+                <img
+                  className={classNames(
+                    "h-24 shadow rouded border-2",
+                    `gallery-image-${i}`,
+                    {
+                      "border-purple-100": i !== index,
+                      "border-white": i === index
+                    }
+                  )}
+                  src={image}
+                />
+              </li>
+            ))}
+          </ul>
         </div>
-      ) : null}
+      </div>
     </div>
   );
 };
