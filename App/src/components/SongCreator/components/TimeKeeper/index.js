3c7dee96a3f356d10d723af32c389baf7d22d533
mykeels <mykehell123@gmail.com>
2022-10-15 15:00:56 -0400

    feat: restyle timekeeper

--- App/src/components/SongCreator/components/TimeKeeper/index.js
+++ App/src/components/SongCreator/components/TimeKeeper/index.js
@@ -60,58 +60,47 @@ export const TimeKeeper = ({
   useEffect(() => {
     setMs(value * 1000);
   }, [value]);
+  const isPlaying = !!clock;
   return (
-    <div className="flex w-full text-center items-center justify-center py-2">
-      <button className={classNames("mx-4 record")} onClick={() => record()}>
-        {recordMs !== null ? (
-          <span className="inline-block bg-red-500 text-center text-white px-4 py-2 text-xs rounded shadow">
-            Tap!
-          </span>
-        ) : (
-          <span className="text-2xl">⏺️</span>
-        )}
-      </button>
-      {!clock ? (
-        <button
-          className="text-4xl mx-4 play"
-          onClick={() => start()}
+    <div className="block w-full px-2 py-2">
+      <button
+        className={classNames("record")}
+        onClick={() => record()}
+        disabled={isPlaying && !isRecording}
+      >
+        <span
+          className={classNames(
+            "inline-block text-center text-white px-4 py-2 text-xs rounded shadow hover:bg-purple-200",
+            {
+              "bg-purple-200": isRecording,
+              "bg-purple-100": !isRecording,
+              "bg-pink cursor-not-allowed": isPlaying && !isRecording
+            }
+          )}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-8 h-8"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
-            />
-          </svg>
-        </button>
-      ) : null}
-      {clock ? (
-        <button className="text-4xl mx-4 stop" onClick={() => stop()}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="#000"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-10 h-10"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z"
-            />
-          </svg>
-        </button>
-      ) : null}
+          {isRecording ? "Next Line" : "Record ⏺️"}
+        </span>
+      </button>
+      <button
+        className="px-2 play"
+        onClick={() => (isPlaying ? stop() : start())}
+      >
+        <span
+          className={classNames(
+            "inline-block text-center text-white px-4 py-2 text-xs rounded shadow hover:bg-purple-200",
+            {
+              "bg-purple-200": isPlaying,
+              "bg-purple-100": !isPlaying
+            }
+          )}
+        >
+          {isPlaying ? "Stop" : "Play"}
+        </span>
+      </button>
 
-      {DateTime.fromMillis(ms).toFormat("mm:ss:SSS")}
+      <span className="inline-block float-right text-purple-200 text-lg font-bold px-4">
+        {DateTime.fromMillis(ms).toFormat("mm:ss:SSS")}
+      </span>
     </div>
   );
 };
