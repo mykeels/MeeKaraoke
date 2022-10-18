810f97c8790b1478c3649b08711cba9972f38fce
mykeels <mykehell123@gmail.com>
2022-10-18 02:20:38 -0400

    fix: ci directory

--- DesktopApp/scripts/build-releases.sh
+++ DesktopApp/scripts/build-releases.sh
@@ -12,6 +12,6 @@ dotnet publish -c Release -o bin/Release/net6.0/linux-x64 -p:PublishReadyToRun=t
 dotnet publish -c Release -o bin/Release/net6.0/osx-x64 -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:RuntimeIdentifier=osx-x64
 
 # zip
-cd bin/Release/net6.0/win-x64; zip -r ../../win-x64.zip . -i wwwroot/* MeeKaraoke.exe
-cd bin/Release/net6.0/linux-x64; zip -r ../../linux-x64.zip . -i wwwroot/* MeeKaraoke
-cd bin/Release/net6.0/osx-x64; zip -r ../../osx-x64.zip . -i wwwroot/* MeeKaraoke
+cd $DIR/../bin/Release/net6.0/win-x64; zip -r ../../win-x64.zip . -i wwwroot/* MeeKaraoke.exe
+cd $DIR/../bin/Release/net6.0/linux-x64; zip -r ../../linux-x64.zip . -i wwwroot/* MeeKaraoke
+cd $DIR/../bin/Release/net6.0/osx-x64; zip -r ../../osx-x64.zip . -i wwwroot/* MeeKaraoke
