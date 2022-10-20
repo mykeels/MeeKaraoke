eead92ee65cdd472cfe38be0d0067a1f87ddddea
mykeels <mykehell123@gmail.com>
2022-10-19 23:12:27 -0400

    fix: use functions in build script

--- DesktopApp/scripts/build-releases.sh
+++ DesktopApp/scripts/build-releases.sh
@@ -2,23 +2,37 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-cd $DIR/../
+function build-simple
+{
+    RUNTIME=$1
+    echo "Building $RUNTIME"
+    cd $DIR/../
+    dotnet build -c Release -r $RUNTIME --no-self-contained
+    ls $DIR/../bin/Release/net6.0/win-x64
+    echo "Zip $RUNTIME"
+    cd $DIR/../bin/Release/net6.0/$RUNTIME; zip -r ../../$RUNTIME.zip *
+    echo "---------------------------------------------------------------------"
+}
 
-# publish
-dotnet build -c Release -r win-x64 --no-self-contained
-dotnet build -c Release -r linux-x64 --no-self-contained
-dotnet build -c Release -r osx-x64 --no-self-contained
+function build-self-contained
+{
+    RUNTIME=$1
+    echo "Building $RUNTIME-self-contained"
+    cd $DIR/../
+    dotnet publish -c Release -o bin/Release/net6.0/$RUNTIME-self-contained \
+        -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true \
+        --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true \
+        -p:RuntimeIdentifier=$RUNTIME
+    ls $DIR/../bin/Release/net6.0/$RUNTIME-self-contained
+    echo "Zip $RUNTIME-self-contained"
+    cd $DIR/../bin/Release/net6.0/$RUNTIME-self-contained; zip -r ../../$RUNTIME-self-contained.zip *
+    echo "---------------------------------------------------------------------"
+}
 
-# zip
-cd $DIR/../bin/Release/net6.0/win-x64; zip -r ../../win-x64.zip *
-cd $DIR/../bin/Release/net6.0/linux-x64; zip -r ../../linux-x64.zip *
-cd $DIR/../bin/Release/net6.0/osx-x64; zip -r ../../osx-x64.zip *
 
-cd $DIR/../
+build-simple win-x64
+build-simple linux-x64
+build-simple osx-x64
 
-dotnet publish -c Release -o bin/Release/net6.0/win-x64-self-con -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:RuntimeIdentifier=win-x64
-#dotnet publish -c Release -o bin/Release/net6.0/linux-x64 -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:RuntimeIdentifier=linux-x64
-dotnet publish -c Release -o bin/Release/net6.0/osx-x64-self-con -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:RuntimeIdentifier=osx-x64
-
-cd $DIR/../bin/Release/net6.0/win-x64-self-con; zip -r ../../win-x64-self-contained.zip *
-cd $DIR/../bin/Release/net6.0/osx-x64-self-con; zip -r ../../osx-x64-self-contained.zip *
+build-self-contained win-x64
+build-self-contained osx-x64
