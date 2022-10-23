73ad797f8f64c7b5c7678b36473709c15b7d74f2
mykeels <mykehell123@gmail.com>
2022-10-23 04:14:52 -0400

    fix: make song-picker responsive

--- App/src/components/SongPicker/index.js
+++ App/src/components/SongPicker/index.js
@@ -44,12 +44,12 @@ export const SongPicker = ({
   );
 
   return (
-    <div className="block h-screen w-screen title-creator px-16 py-8">
-      <div className="flex w-full h-full items-center justify-center py-4 px-8 bg-purple-200 opacity-75">
-        <div className="flex w-full h-full text-white">
+    <div className="block h-screen w-screen title-creator px-4 md:px-16 py-8">
+      <div className="flex w-full h-full items-center justify-center py-4 px-4 md:px-8 bg-purple-200 opacity-75 overflow-auto custom-scroller">
+        <div className="block md:flex w-full md:h-full text-white">
           <div
             className={classNames(
-              "flex flex-col h-full w-5/6 py-8 px-4 overflow-auto song-picker-scroller",
+              "flex flex-col h-full w-full md:w-5/6 py-8 px-4 overflow-auto song-picker-scroller",
               {
                 "justify-center items-center ": songRecords.length <= 3
               }
@@ -62,19 +62,21 @@ export const SongPicker = ({
                 <div className="flex w-full" key={record.id}>
                   <button
                     key={record.id}
-                    className="inline-block w-10/12 text-left bg-purple-100 bg-opacity-75 p-4 my-4 text-lg border border-purple-100 hover:border-white"
+                    className="flex md:inline-block w-10/12 text-left bg-purple-100 bg-opacity-75 p-4 my-4 border border-purple-100 hover:border-white"
                     onClick={() => onSelectSong(record)}
                   >
-                    <div className="block w-full text-right text-sm">
+                    <div className="hidden mf:block w-full text-right text-sm">
                       {DateTime.fromISO(record.updatedAt).toRelative()}
                     </div>
-                    <div className="block w-full">
+                    <div className="block w-full text-sm md:text-lg">
                       {i + 1}. {record.title}
                     </div>
                   </button>
                   <button
-                    onClick={() => onPlaySong(record)}
-                    className="flex w-1/12 justify-center items-center text-center my-4 mx-1 text-purple-200 bg-pink border border-pink hover:border-purple-100"
+                    onClick={() =>
+                      typeof onPlaySong === "function" && onPlaySong(record)
+                    }
+                    className="flex w-1/4 md:w-1/12 justify-center items-center text-center my-4 mx-1 text-purple-200 bg-pink border border-pink hover:border-purple-100"
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -93,7 +95,7 @@ export const SongPicker = ({
                   </button>
                   <button
                     onClick={() => _deleteSong(record.id)}
-                    className="flex w-1/12 justify-center items-center text-center my-4 mx-1 text-purple-200 bg-pink border border-pink hover:border-purple-100"
+                    className="flex w-1/4 md:w-1/12 justify-center items-center text-center my-4 mx-1 text-purple-200 bg-pink border border-pink hover:border-purple-100"
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -114,9 +116,9 @@ export const SongPicker = ({
               ))
             )}
           </div>
-          <div className="flex h-full w-1/6 justify-center items-center px-2">
+          <div className="block w-full md:flex md:w-1/6 md:h-full justify-center items-center px-4 md:px-2">
             <button
-              className="bg-purple-100 px-4 py-4 text-lg border border-purple-100 hover:border-white"
+              className="bg-purple-100 px-4 py-4 w-full md:w-auto text-lg border border-purple-100 hover:border-white"
               onClick={() => onNewSong()}
             >
               New Song
