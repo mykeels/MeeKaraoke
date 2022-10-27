9a27bbdbbf7072667f8ec6fc70deaca6ecd4afa4
mykeels <mykehell123@gmail.com>
2022-10-26 22:40:07 -0400

    feat: render-media script

--- App/scripts/render-media.script.js
+++ App/scripts/render-media.script.js
@@ -1,26 +1,94 @@
 const path = require("path");
 const { renderMedia } = require("@remotion/renderer");
+const commandLineArgs = require("command-line-args");
+const commandLineUsage = require("command-line-usage");
+
+const optionDefinitions = [
+  {
+    name: "rendererUrl",
+    alias: "r",
+    type: String,
+    description: "URL to Video Renderer Module containing compositions"
+  },
+  { name: "out", alias: "o", type: String },
+  { name: "duration", alias: "d", type: Number },
+  {
+    name: "karaokeUrl",
+    alias: "k",
+    type: String,
+    description: "URL to JSON file containing karaoke information"
+  },
+  {
+    name: "help",
+    alias: "h",
+    type: Boolean,
+    description: "Display this usage guide."
+  }
+];
+const options = commandLineArgs(optionDefinitions);
+const usage = commandLineUsage([
+  {
+    header: "MeeKaraoke",
+    content: "Render Karaoke videos made by the MeeKaraoke app"
+  },
+  {
+    header: "Options",
+    optionList: optionDefinitions
+  },
+  {
+    content: "Project home: https://github.com/mykeels/MeeKaraoke"
+  }
+]);
 
 (async () => {
   try {
+    if (options.help) {
+      console.log(usage);
+      return;
+    }
+    const assert = (condition, message) => {
+      if (!condition) {
+        console.assert(condition, message);
+        process.exit(1);
+      }
+    };
+    assert(!!options.out, "Missing command option --out=<filepath.mp4>");
+    assert(!!options.duration, "Missing command option --duration=<number>");
+    assert(!!options.karaokeUrl, "Missing command option --karaokeUrl=<string>");
+    const apiRootUrl =
+      process.env.REACT_APP_API_ROOT || "http://localhost:5000";
+    const serveUrl = options.rendererUrl || `${apiRootUrl}/app/renderer/`;
+    const fps = 60;
+    const outputLocation = path.resolve(options.out);
+    const width = 1280,
+      height = 720;
+    const duration = String(options.duration);
+    const karaokeUrl = options.karaokeUrl;
+    console.log(
+      [
+        `Renderer URL: ${serveUrl}`,
+        `Output Filepath: ${outputLocation}`,
+        `Video Size: ${width}x${height}, FPS: ${fps}, Duration: ${duration}s`,
+        `Karaoke URL: ${karaokeUrl}`
+      ].join("\n")
+    );
     await renderMedia({
-      serveUrl: "http://127.0.0.1:8080/",
-      outputLocation: path.join(__dirname, "../videos/asake-rendered.mp4"),
+      serveUrl,
+      outputLocation,
       composition: {
-        id: "SongURLPlayer",
-        width: 640,
-        height: 320,
-        fps: 60,
-        durationInFrames: 60 * 135
+        id: "SongComposition",
+        width,
+        height,
+        fps,
+        durationInFrames: fps * Number(duration)
       },
       codec: "h264",
       envVariables: {
-        REMOTION_FRAMES_PER_SECOND: "60",
-        REMOTION_VIDEO_URL:
-          "http://localhost:5000/songs/3afbe273-71ec-49a6-92fc-742bd46d65ad",
-        REMOTION_VIDEO_DURATION: "135"
+        REMOTION_FRAMES_PER_SECOND: String(fps),
+        REMOTION_VIDEO_URL: karaokeUrl,
+        REMOTION_VIDEO_DURATION: duration
       },
-      onStart: () => console.log("started"),
+      onStart: () => console.log("Encoding ..."),
       onProgress: ({
         renderedFrames,
         encodedFrames,
@@ -28,10 +96,7 @@ const { renderMedia } = require("@remotion/renderer");
         renderedDoneIn,
         stitchStage
       }) => {
-        if (stitchStage === "encoding") {
-          // First pass, parallel rendering of frames and encoding into video
-          console.log("Encoding...");
-        } else if (stitchStage === "muxing") {
+        if (stitchStage === "muxing") {
           // Second pass, adding audio to the video
           console.log("Muxing audio...");
         }
@@ -51,6 +116,5 @@ const { renderMedia } = require("@remotion/renderer");
     });
   } catch (error) {
     console.error(error);
-    await new Promise((resolve) => setTimeout(resolve, 60000));
   }
 })();
--- App/src/components/SongComposition/index.tsx
+++ App/src/components/SongComposition/index.tsx
@@ -1,19 +1,17 @@
-import '../../index.css';
+import "../../index.css";
 
 import React from "react";
 import { Composition, registerRoot } from "remotion";
 import { frames } from "../../common";
 import { SongURLPlayer } from "./SongURLPlayer";
 
-/**
- * @typedef {object} SongURLCompositionProps
- * @property {any} [className]
- */
+type SongURLCompositionProps = {
+  className?: any;
+};
 
-/**
- * @type {React.FC<SongURLCompositionProps & { [key: string]: any }>}
- */
-export const SongURLComposition = () => {
+export const SongURLComposition: React.FC<
+  SongURLCompositionProps & { [key: string]: any }
+> = () => {
   const duration = Number(
     process.env.REACT_APP_VIDEO_DURATION ||
       process.env.REMOTION_VIDEO_DURATION ||
@@ -21,7 +19,7 @@ export const SongURLComposition = () => {
   );
   return (
     <Composition
-      id="SongURLPlayer"
+      id="SongComposition"
       component={({ url }) => (
         <SongURLPlayer url={url} onDurationChange={() => {}} />
       )}
