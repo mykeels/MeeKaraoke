6434c62b22842f4b37639ce103171c626db7ce07
mykeels <mykehell123@gmail.com>
2022-10-30 14:41:09 -0400

    feat: mv SongCreator to a screen component

--- App/src/App.js
+++ App/src/App.js
@@ -11,6 +11,8 @@ import {
 } from "./components";
 import { LyricsTabView } from "./components/SongCreator/components/LyricsTabView";
 import { getSongById, saveSongFileContents } from "./common/services";
+import { SongCreatorScreen } from "./components/SongCreator";
+import { Redirect } from "./common";
 
 /** @type {React.FC<{ Router?: React.FC<{ basename?: string, children: any }> }>} */
 export const App = () => {
@@ -69,43 +71,6 @@ export const App = () => {
           }
         />
         <Route
-          path="/create/:id"
-          element={
-            state?.audioUrl ? (
-              <SongCreator
-                title={state?.title}
-                url={state?.audioUrl}
-                onReset={() => {
-                  setState(null);
-                  navigate("/");
-                }}
-                defaults={{
-                  song: state?.song,
-                  images: state?.images
-                }}
-                LyricsTabView={(props) => (
-                  <LyricsTabView
-                    {...props}
-                    defaults={{
-                      active: "pretty",
-                      text: state?.lyrics
-                    }}
-                  />
-                )}
-                onSave={(content) => {
-                  content["id"] = state?.id;
-                  return saveSongFileContents({
-                    ...content,
-                    id: state?.id,
-                    audioUrl: state?.audioUrl,
-                    lyrics: ""
-                  }).then((content) => setState({ ...state, id: content.id }));
-                }}
-              />
-            ) : null
-          }
-        />
-        <Route
           path="/create"
           element={
             state?.audioUrl ? (
@@ -139,7 +104,36 @@ export const App = () => {
                   }).then((content) => setState({ ...state, id: content.id }));
                 }}
               />
-            ) : null
+            ) : (
+              <Redirect to="/" />
+            )
+          }
+        />
+        <Route
+          path="/create/:id"
+          element={
+            <SongCreatorScreen
+              SongCreator={(props) => (
+                <SongCreator
+                  {...props}
+                  onReset={() => {
+                    setState(null);
+                    navigate("/");
+                  }}
+                  onSave={(content) => {
+                    content["id"] = state?.id;
+                    return saveSongFileContents({
+                      ...content,
+                      id: state?.id,
+                      audioUrl: state?.audioUrl,
+                      lyrics: ""
+                    }).then((content) =>
+                      setState({ ...state, id: content.id })
+                    );
+                  }}
+                />
+              )}
+            />
           }
         />
       </Routes>
---
+++ App/src/components/SongCreator/SongCreatorScreen.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+
+import { getSongById } from "../../common/services";
+import { LyricsTabView } from "./components/LyricsTabView";
+import { SongCreator } from "./SongCreator";
+
+/**
+ * @typedef {object} SongCreatorScreenProps
+ * @property {any} [className]
+ * @property {React.FC<import("./SongCreator").SongCreatorProps>} [SongCreator]
+ * @property {(id: string) => Promise<SongFileContent>} [getSongById]
+ */
+
+/**
+ * @type {React.FC<SongCreatorScreenProps & { [key: string]: any }>}
+ */
+export const SongCreatorScreen = ({ SongCreator, getSongById }) => {
+  const { id } = useParams();
+  const { data: song } = useQuery(["songs", id], () => getSongById(id));
+  return song ? (
+    <SongCreator
+      url={song.audioUrl}
+      title={song.title}
+      defaults={{
+        song: song?.lines,
+        images: song?.images
+      }}
+      LyricsTabView={(props) => (
+        <LyricsTabView
+          {...props}
+          defaults={{
+            active: "pretty",
+            text: song?.lyrics
+          }}
+        />
+      )}
+    />
+  ) : null;
+};
+
+SongCreatorScreen.defaultProps = {
+  SongCreator,
+  getSongById
+};
