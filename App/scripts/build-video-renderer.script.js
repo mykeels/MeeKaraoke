092955f2854a8912941bcedaa3fa8a5177997bd8
mykeels <mykehell123@gmail.com>
2022-10-31 18:05:37 -0400

    fix: specify correct renderer dir

--- App/scripts/build-video-renderer.script.js
+++ App/scripts/build-video-renderer.script.js
@@ -4,14 +4,18 @@
  */
 
 const path = require("path");
+const fs = require("fs");
 const { bundle } = require("@remotion/bundler");
 const { webpackOverride } = require("../src/webpack-override");
 
 (async () => {
   let progress = 0;
   await bundle({
-    entryPoint: path.join(__dirname, "../src/components/SongComposition/index.tsx"),
-    onProgress: p => {
+    entryPoint: path.join(
+      __dirname,
+      "../src/components/SongComposition/index.tsx"
+    ),
+    onProgress: (p) => {
       if (p > progress) {
         progress = p;
         console.log(`progress: ${progress}%`);
@@ -21,4 +25,14 @@ const { webpackOverride } = require("../src/webpack-override");
     enableCaching: true,
     webpackOverride
   });
+  const indexHtmlFile = path.join(__dirname, "../build/renderer/index.html");
+  const indexHtml = fs.readFileSync(indexHtmlFile, "utf8");
+  fs.writeFileSync(
+    indexHtmlFile,
+    indexHtml.replace(
+      'src="/bundle.js"',
+      `src="${process.env.PUBLIC_URL || ""}/bundle.js"`
+    ),
+    "utf8"
+  );
 })();
