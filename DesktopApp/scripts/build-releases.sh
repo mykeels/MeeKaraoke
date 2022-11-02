312d0e0d0fcbd31ec027522672d09e7f5e0b59dc
mykeels <mykehell123@gmail.com>
2022-11-01 21:51:23 -0400

    fix: use powershell to zip release

--- DesktopApp/scripts/build-releases.sh
+++ DesktopApp/scripts/build-releases.sh
@@ -23,13 +23,18 @@ function build-self-contained
         -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true \
         --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true \
         -p:RuntimeIdentifier=$RUNTIME
+    chmod 755 $DIR/../bin/Release/net6.0/$RUNTIME-self-contained/MeeKaraoke
     cp -R $DIR/../Resources $DIR/../bin/Release/net6.0/$RUNTIME-self-contained
-    cp -R $DIR/../bin/Release/net6.0/win-x64/wwwroot $DIR/../bin/Release/net6.0/$RUNTIME-self-contained
+    cp -R $DIR/../../App/build $DIR/../bin/Release/net6.0/$RUNTIME-self-contained
+    mv $DIR/../bin/Release/net6.0/$RUNTIME-self-contained/build $DIR/../bin/Release/net6.0/$RUNTIME-self-contained/wwwroot
     ls $DIR/../bin/Release/net6.0/$RUNTIME-self-contained
+
     echo "Zip $RUNTIME-self-contained"
-    cd $DIR/../bin/Release/net6.0/$RUNTIME-self-contained; zip -r ../../$RUNTIME-self-contained.zip *
+    cd $DIR/../bin/Release/net6.0/$RUNTIME-self-contained
+    powershell.exe -NoLogo -ExecutionPolicy Bypass -Command "Compress-Archive -Path .\* -DestinationPath ../../$RUNTIME-self-contained.zip"
+    #zip -r ../../$RUNTIME-self-contained.zip *
     echo "---------------------------------------------------------------------"
 }
 
-build-self-contained linux-x64
+#build-self-contained linux-x64
 build-self-contained osx-x64
