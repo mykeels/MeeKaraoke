e4bba2521913240940e99ff5886b8ddbd2042d15
mykeels <mykehell123@gmail.com>
2023-01-25 22:46:08 -0500

    fix: module fed

--- App/config-overrides.js
+++ App/config-overrides.js
@@ -9,34 +9,34 @@ module.exports = function override(config, env) {
   config.output.publicPath = "auto";
 
   config.target = "web";
-  config.entry = ["./src/index.tsx"];
+  config.entry = ["./src/index.ts"];
 
-  // config.plugins.push(
-  //   new ModuleFederationPlugin({
-  //     name: "MeeKaraoke",
-  //     filename: "remoteEntry.js",
-  //     exposes: {
-  //       "./index.js": "./src/bootstrap.js"
-  //     },
-  //     shared: {
-  //       react: {
-  //         requiredVersion: deps["react"],
-  //         import: "react",
-  //         shareKey: "react",
-  //         shareScope: "default",
-  //         singleton: true
-  //       },
-  //       "react-dom": {
-  //         requiredVersion: deps["react-dom"],
-  //         singleton: true
-  //       },
-  //       "react-query": {
-  //         requiredVersion: deps["react-query"],
-  //         singleton: true
-  //       }
-  //     }
-  //   })
-  // );
+  config.plugins.push(
+    new ModuleFederationPlugin({
+      name: "MeeKaraoke",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./index.js": "./src/bootstrap.tsx"
+      },
+      shared: {
+        react: {
+          requiredVersion: deps["react"],
+          import: "react",
+          shareKey: "react",
+          shareScope: "default",
+          singleton: true
+        },
+        "react-dom": {
+          requiredVersion: deps["react-dom"],
+          singleton: true
+        },
+        "react-query": {
+          requiredVersion: deps["react-query"],
+          singleton: true
+        }
+      }
+    })
+  );
   config.resolve.fallback = {
     https: false,
     http: false,
